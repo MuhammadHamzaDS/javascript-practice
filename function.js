a845f8a5cc49428228583e79f0180b551c574284
MuhammadHamzaDS <hamzaali709x@gmail.com>
2025-09-23 22:49:34 -0700

    Updated

---
+++ function.js
@@ -0,0 +1,42 @@
+// // // simple but compact function to understand the basic syntax of function
+// // console.log("#####################################################################################################################");
+// // console.log("This Is Function Page:");
+// // function person(){
+// //     console.log("My name is Hamza");
+// // }
+// // // hamza();
+// // name = prompt("Enter your Name");
+// // store = prompt("Enter your Store Name");
+// // console.log(`Welcome ${name} to your branded ${store} store`);
+
+// // // function userdetail(name,store){
+// // //     console.log(`Welcome :${name} to your Branded  ${store} Store`);    
+// // // }
+
+// // // userdetail(name,store);
+
+// console.log("#####################################################################################################################");
+// console.log("This is  Arrow Function Page");
+
+// const sum = (a,b) => {
+// console.log(a+b);
+// };
+// const sub = (a,b) =>
+// {
+//     console.log(a-b);
+
+// };
+// // to store a function in a variable
+// const divide = function (b,c) 
+//     {
+//     console.log(b/c);
+// }
+// function Multiplegreet(func,n){
+//     for (let i =1; i<=n;i++){
+//         func();
+//     }
+
+// }
+// let greet = function(){
+//     console.log("Hello");
+// }
--- object.js
+++ object.js
@@ -1,54 +1,62 @@
-const student = {
-    name: "Hamza",
-    age: 21,
-    marks: 95.7,
-    city: "Sahiwal",
-    colors: ["red", "black"]
+// console.log("#####################################################################################################################");
+// console.log("This is Object Page:");
 
-};
+// const student = {
+//     name: "Hamza",
+//     age: 21,
+//     marks: 1053,
+//     city: "Sahiwal",
+//     colors: ["red", "black"],
+//     // Use of "This"Keyword in Object
+//     getpercentage(){
+//         let percent = ((this.marks/1100)*100);
+//         console.log(`Percentage of ${this.name} is : ${percent}%`);
+//     }
+
+// };
 // There is no specific order in object like array
-classinfo = {
+// classinfo = {
 
-    hamza: {
-        grade : "A+",
-        city : "Sahiwal"
-    },
-    arham: {
-        Grade : "B+",
-        City: "Alif Soo"
+//     hamza: {
+//         grade : "A+",
+//         city : "Sahiwal"
+//     },
+//     arham: {
+//         Grade : "B+",
+//         City: "Alif Soo"
 
-    },
-    anas: {
-        grade : "A+",
-        City :"Bilal Colony"
-    }
+//     },
+//     anas: {
+//         grade : "A+",
+//         City :"Bilal Colony"
+//     }
 
 
-}; 
-// arrays of objects
-classinfo1 =[
-    { 
-        name:"Hassan",
-        age:23,
-        city:"Tariq-bin Ziad Colony"
+// }; 
+// // arrays of objects
+// classinfo1 =[
+//     { 
+//         name:"Hassan",
+//         age:23,
+//         city:"Tariq-bin Ziad Colony"
     
-    },
-    {
-        name :"Daniyal",
-        age: 24,
-        city:"okara"
-    },
-    {
-        name :"Hanzala",
-        city:"bhawalpur",
-        age:25
-        }
-]
+//     },
+//     {
+//         name :"Daniyal",
+//         age: 24,
+//         city:"okara"
+//     },
+//     {
+//         name :"Hanzala",
+//         city:"bhawalpur",
+//         age:25
+//         }
+// ]
 
-for(let i=0;i<3;i++)
+// for(let i=0;i<3;i++)
 
-{
-    console.log(`This is  Array ${i}` );
-    console.log(` Age of ${classinfo1[i].name}  is:${classinfo1[i].age}\n`);
-    console.log(` City of ${classinfo1[i].name}  is:${classinfo1[i].city    }`);
-}
+// {
+//     console.log(`This is  Array ${i}` );
+//     console.log(` Age of ${classinfo1[i].name}  is:${classinfo1[i].age}\n`);
+//     console.log(` City of ${classinfo1[i].name}  is:${classinfo1[i].city    }`);
+// }
