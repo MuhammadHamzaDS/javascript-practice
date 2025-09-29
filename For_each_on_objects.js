e5dfacfe5b34484c5e5872c1641452afd6f51707
MuhammadHamzaDS <hamzaali709x@gmail.com>
2025-09-29 09:31:24 -0700

    Update

--- For_each_on_objects.js
+++ For_each_on_objects.js
@@ -1,13 +1,13 @@
-// let array = [
-//     {
-//         name: "Sir Jameel",
-//         age: 38,
-//     },
-//     {
-//         name: "Muhammad Hamza",
-//         age: 22
-//     }
-// ]
-// array.forEach((array) =>{
-// console.log(`Name of the Users On Instagram are :${array.name}`);
-// });
+let array = [
+    {
+        name: "Sir Jameel",
+        age: 38,
+    },
+    {
+        name: "Muhammad Hamza",
+        age: 22
+    }
+]
+array.forEach((array) =>{
+console.log(`Name of the Users On Instagram are :${array.name}`);
+});
