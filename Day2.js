8d0fc533a4ecaead3b5f90b2be34938abefa2d39
MuhammadHamzaDS <hamzaali709x@gmail.com>
2025-09-29 09:32:37 -0700

    Updated

--- Day2.js
+++ Day2.js
@@ -2,5 +2,5 @@ let arr =['hamza','ali','usman',40,50];
 let hamza = ['khan','hamza','ali','usman'];
 let bhai = [1,23,445,6,66];
 let oupt = bhai.includes(23);
-console.log(`The Given Number is ${oupt}`);
- 
+console.log(`Output of the Number is: ${oupt}`);
+    
