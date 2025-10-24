0010b3eb85bb802b3a00d978f2526e9c6eb303e1
MuhammadHamzaDS <hamzaali709x@gmail.com>
2025-10-24 06:14:55 +0500

    Updated

--- higher_order_func.js
+++ higher_order_func.js
@@ -1,20 +1,20 @@
-// function oddoreven(request) {
-//     if (request == "odd"){
+function oddoreven(request) {
+    if (request == "odd"){
 
-//         return odd = function (n) {
+        return odd = function (n) {
 
-//             console.log(!(n % 2 == 0));
+            console.log(!(n % 2 == 0));
 
-//         }
-//     };
+        }
+    };
 
-//     elseif(request == "even");{
-//         return even = function (n) {
-//             console.log(n % 2 == 0);
-//         }
-//     }
-//     // else
-//     // {
-//     //     console.log("You enter a invalid Number");
-//     // }
-// }
+    elseif(request == "even");{
+        return even = function (n) {
+            console.log(n % 2 == 0);
+        }
+    }
+    // else
+    // {
+    //     console.log("You enter a invalid Number");
+    // }
+}
--- set_interval.js
+++ set_interval.js
@@ -1,3 +1,7 @@
 // let id = setInterval(()=>{
 //     console.log("Hi Welcome To my Channel:")
 // },2000);
+
+let id = setInterval(()=>{
+    console.log("This is Javascript coding Dojo:")
+},3000);
